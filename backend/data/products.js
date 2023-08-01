e9f43daf3ff263b791bde955201fea6733873a83
OussamaRguez <oussema.rguez@gmail.com>
2023-08-01 17:36:02 +0200

    third commit

--- backend/data/products.js
+++ backend/data/products.js
@@ -17,10 +17,10 @@ const products = [
     name: "iPhone 14 Pro 256GB Memory",
     image: "/images/phone.jpg",
     description:
-      "Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life",
+      "Introducing the iPhone 14 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life",
     brand: "Apple",
     category: "Electronics",
-    price: 599.99,
+    price: 849.99,
     countInStock: 7,
     rating: 4.0,
     numReviews: 8,
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Row, Col } from "react-bootstrap";
-import products from "../products";
 import Product from "../components/Product";
+import axios from "axios";
 
-function HomeScreen() {
+const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <h1>Latest Products</h1>
@@ -16,6 +26,6 @@ function HomeScreen() {
       </Row>
     </>
   );
-}
+};
 
 export default HomeScreen;
